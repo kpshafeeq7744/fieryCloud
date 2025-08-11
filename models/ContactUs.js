8b933d572cd5bad924226baf887ae60e2b92a433
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2025-08-11 08:52:02 +0530

    update 2-

--- models/ContactUs.js
+++ models/ContactUs.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const contactUsSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  phoneNumber: { type: String, },
+  phoneNumber: { type: String, required: true },
   message: { type: String, required: true },
   vendor:{type:String,required:true}
 }, { timestamps: true });
